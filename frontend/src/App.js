e03540d6a524ea65d8799dd28a33de994eb44ffb
srinidhids15 <148432898+srinidhids15@users.noreply.github.com>
2025-11-13 12:59:13 +0530

    error fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 import Home from "./pages/Home";
-import "./styles.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="app-wrap">
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   );
 }
