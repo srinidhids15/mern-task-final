ce2e79db0e1c54812c7a14f09e593e390ac19c5d
srinidhids15 <148432898+srinidhids15@users.noreply.github.com>
2025-11-13 12:57:34 +0530

    changews

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Home from "./pages/Home";
+import "./styles.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
+      <div className="app-wrap">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
